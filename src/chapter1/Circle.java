875662a146f72b2d9135bc10fca6f7381c3a53f4
王玉阳 <epis2048@outlook.com>
2020-09-18 15:37:15 +0800

    add

---
+++ src/chapter1/Circle.java
@@ -0,0 +1,17 @@
+public class Circle {
+    private int r;
+    private int x;
+    private int y;
+    private boolean isVisible;
+    private String color;
+
+    public Circle() {
+        x = 0;
+        y = 0;
+        r = 1;
+        isVisible = true;
+        color = "blue";
+    }
+
+
+}
---
+++ src/chapter1/Square.java
@@ -0,0 +1,12 @@
+public class Square {
+    private int a;
+    private int x;
+    private int y;
+    private String color;
+    private boolean isVisible;
+
+    public Square(){
+
+    }
+
+}
