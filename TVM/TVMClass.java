739273b4235d853ac6e765a8330b81a2810ac510
王玉阳 <epis2048@outlook.com>
2020-10-03 16:14:03 +0800

    chapter1

---
+++ TVM/TVMClass.java
@@ -0,0 +1,52 @@
+public class TVM { //自动售票机 Ticket Vending Machine
+    private int price;
+    private int balance;
+    private int total;
+
+    public void TVM() {
+        price = 2;
+        balance = 0;
+        total = 0;
+    }
+
+    public void setPrice(int inputPrice) {
+        this.price = inputPrice;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void addBalance(int inputBalance) {
+        this.balance += inputBalance;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public int getTotal() {
+        return this.total;
+    }
+
+    public void getTicket() {
+        if (balance >= price) {
+            System.out.println("车票：" + price + "元");
+            balance -= price;
+            total += price;
+        } else {
+            System.out.println("余额不足，出票失败！");
+        }
+    }
+
+    public int refond() {
+        int temp = balance;
+        balance = 0;
+        return temp;
+    }
+
+    public void reset() {
+        balance = 0;
+        total = 0;
+    }
+}
---
+++ TVM/TVMConsole.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class TVMConsole{
+    public static void main(String[] args) {
+        TVM tvm1 = new TVM();
+        if(args.length > 0){
+            tvm1.setPrice(Integer.parseInt(args[0]));
+        }
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            boolean quitFlag = false;
+            System.out.println("===============欢迎使用自动售票机===============");
+            System.out.println("本机销售固定票价 " + price + " 元的车票");
+            while (true) {
+                System.out.println("请选择服务...");
+                System.out.println("1、投币");
+                System.out.println("2、打印车票");
+                System.out.println("3、找零");
+                System.out.println("reset 重置");
+                System.out.println("turn off 关机");
+                String input = scanner.nextLine();
+                switch (input) {
+                    case "1":
+                        int amount = scanner.nextInt();
+                        scanner.nextLine();
+                        if (amount < 0) {
+                            System.out.println("请正常投币...");
+                        } else {
+                            ticketMachine.addMoney(amount);
+                        }
+                        System.out.println("当前余额：" + ticketMachine.getBalance() + " 元");
+                        break;
+                    case "2":
+                        ticketMachine.print();
+                        System.out.println("当前余额：" + ticketMachine.getBalance() + " 元");
+                        if (ticketMachine.getBalance() == 0) {
+                            quitFlag = true;
+                        }
+                        break;
+                    case "3":
+                        System.out.println("请收好找零 " + ticketMachine.refund() + " 元");
+                        quitFlag = true;
+                        break;
+                    case "reset":
+                        System.out.println("本次运营总收入 " + ticketMachine.getTotal() + " 元");
+                        ticketMachine.reset();
+                        System.out.println("============本机被重置============");
+                        quitFlag = true;
+                        break;
+                    case "turn off":
+                        System.exit(0);
+                    default:
+                        System.out.println("请输入正确的功能序号");
+                }
+                if (quitFlag) {
+                    break;
+                }
+            }
+        }
+    }
+}
